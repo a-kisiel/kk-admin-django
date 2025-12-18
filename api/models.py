7e5af1e1ad14b15970650e2b5a8d672004f19bb8
Alex Kisiel <arkisiel01@gmail.com>
2025-12-18 00:55:48 -0500

    Update migrations; Remove db

--- api/models.py
+++ api/models.py
@@ -3,9 +3,9 @@ from django.db import models
 class Collection(models.Model):
     title = models.CharField(max_length=55)
     location = models.CharField(max_length=65, blank=True, null=True)
-    start_date = models.CharField()
-    end_date = models.CharField(blank=True, null=True)
-    description = models.CharField(blank=True, null=True)
+    start_date = models.CharField(max_length=65)
+    end_date = models.CharField(blank=True, null=True, max_length=65)
+    description = models.CharField(blank=True, null=True, max_length=255)
     active = models.BooleanField(default=True)
 
 class Medium(models.Model):
@@ -14,7 +14,7 @@ class Medium(models.Model):
 class Piece(models.Model):
     title = models.CharField(max_length=60)
     filename = models.CharField(max_length=60, blank=True, null=True)
-    date = models.CharField(default=None, blank=True, null=True)
+    date = models.CharField(default=None, blank=True, null=True, max_length=65)
     description = models.CharField(max_length=255, blank=True, null=True)
     collections = models.ManyToManyField(Collection, blank=True)
     media = models.ManyToManyField(Medium)
